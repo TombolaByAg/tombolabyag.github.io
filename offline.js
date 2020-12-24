40dae02828b0b895bafd86f966943d8c21336c72
Alessio Giordano <dev@alessiogiordano.net>
2020-12-24 18:02:53 +0100

    v2 Website Update
    - The website is now able to display the meeting url if present
    - English localization
    - New color for links and new app icon
    - The state of the closed cells is preserved across page reloads
    - Offline functionality via the service worker

---
+++ offline.js
@@ -0,0 +1,39 @@
+// Get Cache
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open("Tombola v2.0").then(function(cache) {
+      return cache.addAll(
+        [
+          // Main App
+          '/index.html',
+          '/style.css',
+          '/script.js',
+          // 404 Error
+          '/404.html',
+          // Privacy Policy
+          '/privacy/index.html',
+          // Favicons
+          '/site.webmanifest',
+          '/safari-pinned-tab.svg',
+          '/favicon-16x16.png',
+          '/favicon-32x32.png',
+          '/favicon.ico',
+          '/android-chrome-192x192.png',
+          '/android-chrome-512x512.png',
+          '/touch-icon-iphone.png',
+          '/touch-icon-ipad.png',
+          '/touch-icon-iphone-retina.png',
+          '/touch-icon-ipad-retina.png',
+        ]
+      );
+    })
+  );
+});
+// Serve Cache
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// Register Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('offline.js').then(function(registration) {
+      // Registration was successful
+      console.log('Tombola by AG è ora in grado di funzionare offline', registration.scope);
+      console.log('Tombola by AG can now work offline', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('Tombola by AG non è in grado di funzionare offline', err);
+      console.log('Tombola by AG cannot work offline', err);
+    });
+  });
+}
 // Costanti per la generazione delle cartelle
 const pattern = [
 	[false, true, true, false, false, true, true, true, true],
@@ -16,12 +30,10 @@ const table = [
 	[false, false, true, true, true, false, false, false, true, false, true, true, true, true, false, true, false, true, false, true, false, true, false, true, true, true, false],
 	[true, true, false, false, false, true, true, true, false, true, false, true, false, true, true, false, true, false, true, false, true, false, true, false, true, false, true]
 ];
-
 // Apertura/Chiusura di una cartella
 function toggle(event) {
 	event.currentTarget.classList.toggle("chiuso");
 }
-
 // Generazione dei numeri di una cartella
 function generateNumbers() {
 	var cartella = {
@@ -79,51 +91,147 @@ function populateTable(cartella) {
 		}
 	}
 }
+function internationalizeLinks(lang) {
+	if (lang == "it") {
+		document.querySelector("#appLink").innerText = "Scarica l'app";
+		document.querySelector("#privacyLink").innerText = "Privacy";
+		document.querySelector("#emailLink").innerText = "Contatti";
+	} else {
+		document.querySelector("#appLink").innerText = "Get the App";
+		document.querySelector("#privacyLink").innerText = "Privacy";
+		document.querySelector("#emailLink").innerText = "Contacts";
+	}
+}
+function backupClosed() {
+	var backup = ""
+	let storage = document.querySelector(".cartella").dataset.storage;
+	if (storage != "") {
+		for(var column = 0; column < 9; column++) {
+			for(var row = 0; row < 3; row++) {
+				backup += (document.querySelector("#c" + column + "r" + row).parentElement.classList.contains("chiuso") ? "1" : "0");
+			}
+		}
+		window.localStorage.setItem("progress://" + storage, backup);
+	}
+}
+function restoreClosed() {
+	let storage = document.querySelector(".cartella").dataset.storage;
+	let backup = window.localStorage.getItem("progress://" + storage);
+	var i = 0;
+	if (backup != "") {
+		for(var column = 0; column < 9; column++) {
+			for(var row = 0; row < 3; row++) {
+				if (backup[i] == "1") {
+					document.querySelector("#c" + column + "r" + row).parentElement.classList.add("chiuso")
+				}
+				i += 1;
+				if (i == backup.length) { return }
+			}
+		}
+	}
+}
+function openClosed() {
+	for(var column = 0; column < 9; column++) {
+		for(var row = 0; row < 3; row++) {
+			document.querySelector("#c" + column + "r" + row).parentElement.classList.remove("chiuso")
+		}
+	}
+}
 function loadTable() {
+
+	backupClosed()
+
+	var lang;
+	var language = navigator.language || navigator.userLanguage;
+	switch(language.substring(0,2)) {
+		case "it": document.documentElement.lang = "it"; lang = "it"; break;
+		default: document.documentElement.lang = "en"; lang = "en"; break;
+	}
+	internationalizeLinks(lang);
+
 	var data = location.hash; // #title=Senza-titolo&time=15857373802552261
 	let titleBegin = data.indexOf("#title=");
 	let timeBegin = data.indexOf("&time=");
+	
+	
 	if((titleBegin != -1) && (timeBegin != -1)) {
-		var title = data.substring(titleBegin + 7, timeBegin).replace(/-/g, " ");;
+		
+		let skypeBegin = data.indexOf("&skype=");
+		let jitsiBegin = data.indexOf("&jitsi=");
+		
+		var titleEnd = timeBegin;
+		
+		if (skypeBegin != -1) {
+			titleEnd = skypeBegin;
+		} else if (jitsiBegin != -1) {
+			titleEnd = jitsiBegin;
+		}
+		
+		var title = data.substring(titleBegin + 7, titleEnd).replace(/-/g, " ");
+		document.querySelector("header h1").innerText = title;
 		var timestamp = data.substring(timeBegin + 6, timeBegin + 19);
 		var time = new Date(parseInt(timestamp));
 		
-		document.querySelector("header h1").innerText = title;
+		if (titleEnd != timeBegin) {
+			// Game with video-call
+			var link = data.substring(titleEnd + 7, timeBegin);
+			if (skypeBegin != -1) {
+				if (lang == "it") {
+					document.querySelector("header p").innerHTML = '<a href="https://join.skype.com/' + link + '" target="_blank">Avvia Videochiamata su Skype &gt;</a>';
+				} else {
+					document.querySelector("header p").innerHTML = '<a href="https://join.skype.com/' + link + '" target="_blank">Join Video Call on Skype &gt;</a>';
+				}
+			} else if (jitsiBegin != -1) {
+				if (lang == "it") {
+					document.querySelector("header p").innerHTML = '<a href="https://meet.jit.si/' + link + '" target="_blank">Avvia Videochiamata su Jitsi &gt;</a>';
+				} else {
+					document.querySelector("header p").innerHTML = '<a href="https://meet.jit.si/' + link + '" target="_blank">Join Video Call on Jitsi &gt;</a>';
+				}
+			}
+		} else {
+			// Game with no video-call
 		
-		var month = "";
+			var month = "";
 		
-		switch(time.getMonth()) {
-			case 0: month="Gennaio";break;
-			case 1: month="Febbraio";break;
-			case 2: month="Marzo";break;
-			case 3: month="Aprile";break;
-			case 4: month="Maggio";break;
-			case 5: month="Giugno";break;
-			case 6: month="Luglio";break;
-			case 7: month="Agosto";break;
-			case 8: month="Settembre";break;
-			case 9: month="Ottobre";break;
-			case 10: month="Novembre";break;
-			case 11: month="Dicembre";break;
+			switch(time.getMonth()) {
+				case 0: month=(lang == "it" ? "Gennaio" : "January");break;
+				case 1: month=(lang == "it" ? "Febbraio" : "February");break;
+				case 2: month=(lang == "it" ? "Marzo" : "March");break;
+				case 3: month=(lang == "it" ? "Aprile" : "April");break;
+				case 4: month=(lang == "it" ? "Maggio" : "May");break;
+				case 5: month=(lang == "it" ? "Giugno" : "June");break;
+				case 6: month=(lang == "it" ? "Luglio" : "July");break;
+				case 7: month=(lang == "it" ? "Agosto" : "August");break;
+				case 8: month=(lang == "it" ? "Settembre" : "September");break;
+				case 9: month=(lang == "it" ? "Ottobre" : "October");break;
+				case 10: month=(lang == "it" ? "Novembre" : "November");break;
+				case 11: month=(lang == "it" ? "Dicembre" : "December");break;
+			}
+		
+			var hr = time.getHours();
+			var hours = hr;
+			if(hr < 10) {
+				hours = "0" + hours;
+			}
+			var mn = time.getMinutes();
+			var minutes = mn;
+			if(mn < 10) {
+				minutes = "0" + minutes;
+			}
+			
+			if (lang == "it") {
+				var desc = "Partita creata il " + time.getDate() + " " + month + " " + time.getFullYear() + " alle " + hours + ":" + minutes;
+				document.querySelector("header p").innerText = desc;
+			} else {
+				var desc = "Game created on " + time.getDate() + " " + month + " " + time.getFullYear() + " at " + hours + ":" + minutes;
+				document.querySelector("header p").innerText = desc;
+			}
+			// Next time use .toLocaleDateString()
 		}
-		
-		var hr = time.getHours();
-		var hours = hr;
-		if(hr < 10) {
-			hours = "0" + hours;
-		}
-		var mn = time.getMinutes();
-		var minutes = mn;
-		if(mn < 10) {
-			minutes = "0" + minutes;
-		}
-		
-		
-		var desc = "Partita creata il " + time.getDate() + " " + month + " " + time.getFullYear() + " alle " + hours + ":" + minutes;
-		document.querySelector("header p").innerText = desc;
 		var key = "title=" + title + "&time=" + timestamp;
 		if(window.localStorage.getItem(key) != null) {
 			var cartella = JSON.parse(window.localStorage.getItem(key));
+			document.querySelector(".cartella").dataset.storage = key;
 			try {
 				populateTable(cartella);
 			} catch(error) {
@@ -134,14 +242,18 @@ function loadTable() {
 				populateTable(cartella);
 			}
 		} else {
+			document.querySelector(".cartella").dataset.storage = "";
 			var cartella = generateNumbers();
 			window.localStorage.setItem(key, JSON.stringify(cartella));
 			populateTable(cartella);
 		}
+		openClosed();
+		restoreClosed();
 	} else {
-		document.querySelector("header h1").innerText = "Buona fortuna";
-		document.querySelector("header p").innerText = "Questa cartella è stata generata automaticamente per te";
+		document.querySelector("header h1").innerText = (lang == "it" ? "Buona fortuna" : "Good Luck");
+		document.querySelector("header p").innerText = (lang == "it" ? "Questa cartella è stata generata automaticamente per te" : "This ticket was generated automatically for you");
 		populateTable(generateNumbers());
+		openClosed();
 	}
 	
 }
